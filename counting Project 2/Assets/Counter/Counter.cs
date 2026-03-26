50c070aa1b764d299d7bfa7a96f0523f101f1beb
cking2026 <cking2026@jamesrumsey.net>
2026-03-26 08:45:26 -0400

    changes how much score it counts per ball

--- counting Project 2/Assets/Counter/Counter.cs
+++ counting Project 2/Assets/Counter/Counter.cs
@@ -17,7 +17,7 @@ public class Counter : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        Count += 1;
+        Count += 2;
         CounterText.text = "Count : " + Count;
     }
 }
